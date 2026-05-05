eae125ad5e162373a5cccce7129038c88c1105f2
javi11 <javicuba11@gmail.com>
2026-05-05 08:59:03 +0200

    chore(database): remove obsolete migration files for database schema cleanup

--- internal/database/migrations/018_add_target_path.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE import_queue ADD COLUMN target_path TEXT;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
--- SQLite does not support DROP COLUMN; no-op
--- +goose StatementEnd
--- internal/database/migrations/027_add_metadata_to_file_health.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE file_health ADD COLUMN metadata JSONB DEFAULT NULL;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE file_health DROP COLUMN metadata;
--- +goose StatementEnd
